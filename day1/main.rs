21cfb0cf39fcc59071f7c90b2b2143a8d4550cb5
NCanggoro <ansianggoro@gmail.com>
2020-12-11 22:08:02 +0700

    cleanup

--- day1/main.rs
+++ day1/main.rs
@@ -1,31 +1,3 @@
-fn main() {
-  use std::env::args;
-  use std::fs::read_to_string;
-  use std::time::{Duration, Instant};
-
-  let input_files = 
-              args()
-              .skip(1)
-              .next()
-              .expect("There's no Input File");
-
-  let input = read_to_string(&input_files).unwrap();
-
-  let processed_input = input
-                .split("\n")
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect::<Vec<_>>();
-
-  // println!("{:?}", &processed_input);
-  // println!("input: {}", &input_files);
-
-  // calculate how long to run the function
-  // let start = Instant::now();
-  println!("res: {}", find_sum_better_answer(&processed_input));
-  // let duration = start.elapsed();
-  // println!("Time Elapsed {:?}", duration);
-}
-
 fn find_sum(x: &Vec<i32>) -> i32 {
   let n = x.len();
   for i in 0..n - 2 {
@@ -53,5 +25,33 @@ fn find_sum_better_answer(x:&Vec<i32>) -> i32 {
       }
     }
   }
-  return unreachable!();
+  unreachable!();
+}
+
+fn main() {
+  use std::env::args;
+  use std::fs::read_to_string;
+  use std::time::{Duration, Instant};
+
+  let input_files = 
+              args()
+              .skip(1)
+              .next()
+              .expect("There's no Input File");
+
+  let input = read_to_string(&input_files).unwrap();
+
+  let processed_input = input
+                .split("\n")
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+  // println!("{:?}", &processed_input);
+  // println!("input: {}", &input_files);
+
+  // calculate how long to run the function
+  // let start = Instant::now();
+  println!("res: {}", find_sum_better_answer(&processed_input));
+  // let duration = start.elapsed();
+  // println!("Time Elapsed {:?}", duration);
 }
