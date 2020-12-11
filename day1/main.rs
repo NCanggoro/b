e3042258be22e425d32a5df2dc8252d18c2d3eb9
NCanggoro <ansianggoro@gmail.com>
2020-12-11 22:00:47 +0700

    update

--- day1/main.rs
+++ day1/main.rs
@@ -21,7 +21,7 @@ fn main() {
 
   // calculate how long to run the function
   // let start = Instant::now();
-  println!("res: {}", find_sum(&processed_input));
+  println!("res: {}", find_sum_better_answer(&processed_input));
   // let duration = start.elapsed();
   // println!("Time Elapsed {:?}", duration);
 }
@@ -29,14 +29,29 @@ fn main() {
 fn find_sum(x: &Vec<i32>) -> i32 {
   let n = x.len();
   for i in 0..n - 2 {
-    for j in i + 1..n - 1 {
-      for k in i + 1..n {
+    for j in i + 1..n - 1 { //n²
+      for k in i + 1..n { // n³
         if x[i] + x[j] + x[k] == 2020 {
           return x[i] * x[j] * x[k];
         }
       }
     }
   }
-
   unreachable!();
+}
+
+// better answer i found from a website (faster)
+fn find_sum_better_answer(x:&Vec<i32>) -> i32 {
+  // https://stackoverflow.com/questions/47618823/cannot-borrow-as-mutable-because-it-is-also-borrowed-as-immutable
+  let mut xtemp = x.clone();
+  let n = xtemp.len();
+  xtemp.sort();
+  for i in 0..n - 1 {
+    if let Ok(j) = xtemp.binary_search(&(2020 - xtemp[i])) {
+      if i != j {
+        return xtemp[i] * xtemp[j];
+      }
+    }
+  }
+  return unreachable!();
 }
