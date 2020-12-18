f2e6487543e91250a602e0fe944c0684876a1e30
NCanggoro <ansianggoro@gmail.com>
2020-12-18 20:36:25 +0700

    2nd part

--- day2/main.rs
+++ day2/main.rs
@@ -1,22 +1,60 @@
 fn is_valid(mut x: &str) -> bool {
 	let mut vec = x
-					.split_whitespace()
-					.collect::<Vec<&str>>();
+				.split_whitespace()
+				.collect::<Vec<&str>>();
   let temp = vec[0]
-          .split("-")
-          .map(|t| t.parse::<usize>().unwrap())
-          .collect::<Vec<_>>();
+				.split("-")
+				.map(|t| t.parse::<usize>().unwrap())
+				.collect::<Vec<_>>();
   let temp2 = vec[1]
-          .replace(":", "");
-  let matched = vec[2].matches(&temp2).count();
+				.replace(":", "");
+	let matched = vec[2]
+				.matches(&temp2)
+				.count();
 	if(&matched >= &temp[0] && &matched <= &temp[1]) {
 		return true
 	} else {
 		return false;
 	}
+	unreachable!();
 }
 
-// fn is_valid_2()
+fn is_valid_2(mut x: &str) -> bool {
+	let mut total = 0;
+	let mut vec = x
+				.split_whitespace()
+				.collect::<Vec<&str>>();
+  let temp = vec[0]
+				.split("-")
+				.map(|t| t.parse::<usize>().unwrap())
+				.collect::<Vec<_>>();
+	let temp2 = vec[1]
+				.replace(":", "");
+	// println!("{:?}", temp2.chars().nth(0).unwrap());
+	let matched = vec[2]
+				.matches(&temp2)
+				.count();
+	let chars: Vec<char> = vec[2]
+								.chars()
+								.collect();
+	for i in 0..2  {
+		// println!("{:?}", chars.len());
+		// println!("{:?}", chars);
+		if(temp[i] > chars.len()) {
+			if(temp2.chars().nth(0).unwrap() == chars[temp[i] -2 ]) {
+				total += 1;
+			}
+		} else if(temp2.chars().nth(0).unwrap() == chars[temp[i] -1 ]) {
+			total += 1;
+		}
+	}
+
+	if(total == 1 ) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 fn main() {
   use std::env::args;
@@ -38,12 +76,12 @@ fn main() {
   // let test_pass = "1-3 a: abcdefg";
 	// println!("{:?}", &processed_input);
 	let arr = &processed_input.len();
-
+	// println!("{}", is_valid_2(processed_input[0]));
 	for i in 0..*arr{
-		if (is_valid(&processed_input[i])) {
-			x += 1
+		if (is_valid_2(&processed_input[i])) {
+			x += 1;
 		} 
 	}
-	println!("{}", x);
+	println!("resn {}", x);
 
 } 
