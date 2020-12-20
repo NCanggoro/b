e9d4f207759775ffde47ce351e1a7ff91b922ad9
NCanggoro <ansianggoro@gmail.com>
2020-12-20 10:21:17 +0700

    update

--- day2/main.rs
+++ day2/main.rs
@@ -1,54 +1,34 @@
-fn is_valid(mut x: &str) -> bool {
-	let mut vec = x
-				.split_whitespace()
-				.collect::<Vec<&str>>();
-  let temp = vec[0]
-				.split("-")
-				.map(|t| t.parse::<usize>().unwrap())
-				.collect::<Vec<_>>();
-  let temp2 = vec[1]
-				.replace(":", "");
-	let matched = vec[2]
-				.matches(&temp2)
+fn is_valid(limit: &Vec<usize>,chars: &str,pass: &str) -> bool {
+	let matched = pass
+				.matches(chars)
 				.count();
-	if(&matched >= &temp[0] && &matched <= &temp[1]) {
-		return true
+	if(&matched >= &limit[0] && &matched <= &limit[1]) {
+		return true;
 	} else {
 		return false;
 	}
 	unreachable!();
 }
 
-fn is_valid_2(mut x: &str) -> bool {
+fn is_valid_2(limit: &Vec<usize>,chars: &str,pass: &str) -> bool {
 	let mut total = 0;
-	let mut vec = x
-				.split_whitespace()
-				.collect::<Vec<&str>>();
-  let temp = vec[0]
-				.split("-")
-				.map(|t| t.parse::<usize>().unwrap())
-				.collect::<Vec<_>>();
-	let temp2 = vec[1]
-				.replace(":", "");
-	let chars: Vec<char> = vec[2]
+	let pass_chars: Vec<char> = pass
 									.chars()
 									.collect();
 	for i in 0..2  {
-		if(temp[i] > chars.len()) {
-			if(temp2.chars().nth(0).unwrap() == chars[temp[i] -2 ]) {
+		if(limit[i] > pass_chars.len()) {
+			if(chars.chars().nth(0).unwrap() == pass_chars[limit[i] -2 ]) {
 				total += 1;
 			}
-		} else if(temp2.chars().nth(0).unwrap() == chars[temp[i] -1 ]) {
+		} else if(chars.chars().nth(0).unwrap() == pass_chars[limit[i] -1 ]) {
 			total += 1;
 		}
 	}
-
 	if(total == 1 ) {
 		return true;
 	} else {
 		return false;
 	}
-
 	unreachable!();
 }
 
@@ -69,12 +49,19 @@ fn main() {
 								.split("\n")
 								.collect::<Vec<_>>();
 
-  // let test_pass = "1-3 a: abcdefg";
-	// println!("{:?}", &processed_input);
 	let arr = &processed_input.len();
-	// println!("{}", is_valid_2(processed_input[0]));
+
 	for i in 0..*arr{
-		if (is_valid_2(&processed_input[i])) {
+		let mut vec = &processed_input[i]
+						.split_whitespace()
+						.collect::<Vec<&str>>();
+		let temp = &vec[0]
+						.split("-")
+						.map(|t| t.parse::<usize>().unwrap())
+						.collect::<Vec<_>>();
+		let temp2 = &vec[1]
+						.replace(":", "");
+		if (is_valid_2(temp, temp2, vec[2])) {
 			x += 1;
 		} 
 	}
