9453c7e95a06507891f1e5f26838856cd0ba8425
NCanggoro <ansianggoro@gmail.com>
2022-07-12 19:36:05 +0700

    add celeng

---
+++ celeng/snail_sort/src/main.rs
@@ -0,0 +1,36 @@
+        [16, 15, 14, 13, 12, 11],
+    ];
+
+    let mut output: Vec<i32> = Vec::new();
+    let mut rows = std::collections::VecDeque::from_iter(input.iter().map(|row| row.iter()));
+    let mut dir = Right;
+    loop {
+        dir = match dir {
+            Right => match rows.pop_front() {
+                Some(top) => {
+                    output.extend(top);
+                    Down
+                }
+                None => break,
+            },
+            Down => {
+                output.extend(rows.iter_mut().flat_map(|row| row.next_back()));
+                Left
+            }
+            Left => match rows.pop_back() {
+                Some(bottom) => {
+                    output.extend(bottom.rev());
+                    Up
+                }
+                None => break,
+            },
+            Up => {
+                output.extend(rows.iter_mut().rev().flat_map(|row| row.next()));
+                Right
+            }
+        }
+    }
+
+    println!("{:?}", output);
+    assert_eq!(output, Vec::from_iter(1..=36));
+}
