b7ccccad7f8191b9e760d87533f39255b16db665
NCanggoro <ansianggoro@gmail.com>
2022-10-10 21:41:12 +0700

    login form response when username or password invalid

--- emale/src/routes/login/get.rs
+++ emale/src/routes/login/get.rs
@@ -1,7 +1,48 @@
-use actix_web::{HttpResponse, http::header::ContentType};
+use actix_web::http::header::ContentType;
+use actix_web::{web, HttpResponse};
 
-pub async fn login_form() -> HttpResponse {
-	HttpResponse::Ok()
-		.content_type(ContentType::html())
-		.body(include_str!("login.html"))
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>,
+}
+
+pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+    let error_html = match query.0.error {
+        None => "".into(),
+        Some(error_message) => format!("<p><i>{error_message}</i></p>"),
+    };
+
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(format!(
+            r#"
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Login</title>
+			</head>
+			<body>
+			{error_html}
+			<form action="/login" method="post">
+				<label>Username</label>
+				<input
+					type="text"
+					placeholder="Username"
+					name="username"
+				>
+				<label>Password</label>
+				<input
+					type="password"
+					placeholder="Password"
+					name="password"
+				>
+				<button type="submit">Login</button>
+			</form>
+			</body>
+			</html>
+		"#
+        ))
 }
--- emale/src/routes/login/post.rs
+++ emale/src/routes/login/post.rs
@@ -1,8 +1,67 @@
-use actix_http::header::LOCATION;
-use actix_web::HttpResponse;
+use actix_http::{header::LOCATION, StatusCode};
+use actix_web::{HttpResponse, web, ResponseError};
+use secrecy::Secret;
+use sqlx::PgPool;
 
-pub async fn login() -> HttpResponse {
-  HttpResponse::SeeOther()
-    .insert_header((LOCATION, "/"))
-    .finish()
+use crate::{authentication::{Credentials, validate_credentials, AuthError}, routes::error_chain_fmt};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+	username: String,
+	password: Secret<String>
+}
+
+#[tracing::instrument(
+	skip(form, pool),
+	fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(
+	form: web::Form<FormData>, 
+	pool: web::Data<PgPool>
+) -> Result<HttpResponse, LoginError> {
+	let credentials = Credentials {
+		username: form.0.username,
+		password: form.0.password
+	};
+
+	tracing::Span::current()
+		.record("username", &tracing::field::display(&credentials.username));
+
+	let user_id = validate_credentials(credentials, &pool)
+		.await
+		.map_err(|e| match e {
+			AuthError::InvalidCredential(_) => LoginError::AuthError(e.into()),
+			AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into())
+		})?;
+	tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+
+	Ok(HttpResponse::SeeOther()
+		.insert_header((LOCATION, "/"))
+		.finish())
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+	#[error("Auth failed")]
+	AuthError(#[source] anyhow::Error),
+	#[error("Something went wrong")]
+	UnexpectedError(#[from] anyhow::Error)
+}
+
+impl std::fmt::Debug for LoginError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		error_chain_fmt(self, f)
+	}
+}
+
+impl ResponseError for LoginError {
+	fn error_response(&self) -> HttpResponse<actix_http::body::BoxBody> {
+		let encoded_error = urlencoding::Encoded::new(self.to_string());
+		HttpResponse::build(self.status_code())
+			.insert_header((LOCATION, format!("/login?error={}", encoded_error)))
+			.finish()
+	}
+	fn status_code(&self) -> StatusCode {
+		StatusCode::SEE_OTHER
+	}
 }
