642d1c184f9b9eabef03b4db2f9855e2200aaf7d
NCanggoro <ansianggoro@gmail.com>
2022-12-12 13:30:44 +0700

    fix user seed

--- emale/migrations/20221016061547_user_seed.sql
+++ emale/migrations/20221016061547_user_seed.sql
@@ -3,5 +3,5 @@ INSERT INTO users (user_id, username, password_hash)
 VALUES (
   'a4e6e7f1-b4bc-41a6-b5d4-c7c9ac92ea7b',
   'bruh',
-  '$argon2id$v=19$m=15000,t=2,p=1$V0eS5Rcbg8n2/Y64Alibrw$OVN6bUPgzX2inCLPM8AjIyUz/lUS8jF+UGdsFI9FIRM'
+  '$argon2id$v=19$m=15000,t=2,p=1$K69+agHFciO5ZGkAC1y4zQ$iSqg190x2O6btEWUPdvxZF9oTgKOHQCmSVkPDZW07Jo'
 )
--- emale/migrations/20221016064101_seed_user_two.sql
+++ emale/migrations/20221016064101_seed_user_two.sql
@@ -4,6 +4,6 @@ INSERT INTO users (user_id, username, password_hash)
 VALUES (
   'a4e6e7f1-b3bc-41a6-b524-c7c9ac92ea7b',
   'deeznuts',
-  '$argon2id$v=19$m=15000,t=2,p=1$FKyf5LElcdaclwAnveySyw$2xk9hozuDZ420oRiLMd/Ir+s/Xf9SYJN6YWKevRC80g'
+  '$argon2id$v=19$m=15000,t=2,p=1$K69+agHFciO5ZGkAC1y4zQ$iSqg190x2O6btEWUPdvxZF9oTgKOHQCmSVkPDZW07Jo'
 )
 
