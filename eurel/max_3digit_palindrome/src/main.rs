1d00fcb0ff2faec47030797675c0c07a0049b10d
NCanggoro <ansianggoro@gmail.com>
2023-11-22 14:49:57 +0700

    update solution

--- eurel/max_3digit_palindrome/src/main.rs
+++ eurel/max_3digit_palindrome/src/main.rs
@@ -4,7 +4,13 @@ fn main() {
     for i in 100..1000 {
         for j in 100..1000 {
             let temp = i * j;
-            let rev = temp.to_string().chars().rev().collect::<String>().parse::<i64>().unwrap();
+            let rev = temp
+              .to_string()
+              .chars()
+              .rev()
+              .collect::<String>()
+              .parse::<i64>()
+              .unwrap();
             if rev == temp {
                 if res < temp {
                     res = temp;
--- eurel/smallest_multiple/src/main.rs
+++ eurel/smallest_multiple/src/main.rs
@@ -1,18 +1,8 @@
 fn main() {
-    let mut num = 1u128;
-    loop {
-        let mut temp = Vec::new();
-        for i in 1..20 {
-            temp.push(num%i);
-        }
-        let temp_max = temp.iter().max().unwrap();
-        if *temp_max == 0 {
-            break;
-        } else {
-            temp.clear();
-        }
-        num += 1;
-    }   
-    
-    println!("{}", num)
+  let result = (1..)
+  .find(|num| (1..=20).all(|i| num % i == 0))
+  .unwrap();
+
+
+  println!("{}", result);
 }
